e096e1a369cd1a4195f720c250dca1f0a57369a5
Calvin Merry <calvin@calvinmerry.com>
2023-05-30 14:25:59 +0100

    Added decoding functionality
    Fixed a bug so that the newIndex is correctly calculated

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,6 +1,4 @@
 
-
-
 const enDeCode = function () {
   const encodeBtn = document.querySelector('#encode')
   const decodeBtn = document.querySelector('#decode')
@@ -8,40 +6,83 @@ const enDeCode = function () {
   const messageBox = document.querySelector('#userMessage')
   const message = document.querySelector('#userMessage').value.toLowerCase();
   const messageArray = message.split('')
+  const userKeyInput = document.querySelector('#userKey')
   const userKey = parseInt(document.querySelector('#userKey').value)
+
   const encodedArray = [];
+  const decodedArray = []
+
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
+  // Prevent page refreshing
+  userKeyInput.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  })
+
+  ////////// ENCODE /////////
+
   // Loop through message array
-  messageArray.forEach(letter => {
-    if (letter === ' ') {
-      encodedArray.push(' ');
-      return;
-    }
+  if (encodeBtn.checked) {
+    messageArray.forEach(letter => {
+      if (letter === ' ') {
+        encodedArray.push(' ');
+        return;
+      }
 
-    let index = alphabet.indexOf(letter);
-    let newIndex = index + userKey;
-    let newLetter = alphabet[newIndex];
+      let index = alphabet.indexOf(letter);
+      let newIndex = index + userKey;
+      let newLetter = alphabet[newIndex];
 
-    // If new index is greater than 25, subtract 26 to get new index (to loop over alphabet)
-    if (newIndex > 25) {
-      newIndex = newIndex - 26;
-      newLetter = alphabet[newIndex];
-    }
+      // If new index is greater than 25, subtract 26 to get new index (to loop over alphabet)
+      if (newIndex > 25) {
+        newIndex = newIndex - 26;
+        newLetter = alphabet[newIndex];
+      }
 
-    // Push new letter to encoded array
-    encodedArray.push(newLetter);
+      // Push new letter to encoded array
+      encodedArray.push(newLetter);
 
-  });
+    });
 
-  // Join encoded array into string
-  const encodedMessage = encodedArray.join('');
+    // Join encoded array into string
+    const encodedMessage = encodedArray.join('');
 
-  // Set output to encoded message
-  output.innerHTML = encodedMessage;
+    // Set output to encoded message
+    output.innerHTML = encodedMessage;
+  }
 
   ////////// DECODE //////////
+  if (decodeBtn.checked) {
+    messageArray.forEach(letter => {
+      if (letter === ' ') {
+        decodedArray.push(' ');
+        return;
+      }
+
+      let index = alphabet.indexOf(letter);
+      let newIndex = index - userKey;
+      let newLetter = alphabet[newIndex];
+
+      // If new index is less than 0, add 26 to get new index (to loop over alphabet)
+      if (newIndex < 0) {
+        newIndex = newIndex + 26;
+        newLetter = alphabet[newIndex];
+      }
+
+      // Push new letter to encoded array
+      decodedArray.push(newLetter);
+
+    });
+
+    // Join encoded array into string
+    const decodedMessage = decodedArray.join('');
+
+    // Set output to encoded message
+    output.innerHTML = decodedMessage;
+  }
 
 }
 
